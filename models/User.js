e24a6d7db9ce8e290f40f690b859ce513b36a184
Евгений Левкович <50261089+evlev777@users.noreply.github.com>
2020-03-13 23:51:16 +0300

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const {Schema,model,Types} = require('mongoose')
+
+const schema = new Schema({
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    date:{type:Date,default:Date.now},
+    notes:[{type:Types.ObjectId,ref:'Note'}]
+})
+
+module.exports = model('User',schema)
